835f6f4db24d5d27e557ca687d1a58fe9ca71786
Aurore DUCOBU <aducobu@paul-f4Br8s1.clusters.42paris.fr>
2024-07-03 18:45:02 +0200

    day0/ex07..

---
+++ day0/ex07/sos.py
@@ -0,0 +1,24 @@
+import sys
+
+NESTED_MORSE = { " ": "/ ", "A": ".- ", "B": "-... ", "C": "-.-. ", "D": "-.. ", "E": ". ",
+                 "F": "..-. ", "G": "--. ", "H": ".... ", "I": ".. ", "J": ".--- ", "K": "-.- ",
+                 "L": ".-.. ", "M": "-- ", "N": "-. ", "O": "--- ", "P": ".--. ", "Q": "--.- ",
+                 "R": ".-. ", "S": "... ", "T": "- ", "U": "..- ", "V": "...- ", "W": ".-- ", "X": "-..- ",
+                 "Y": "-.-- ", "Z": "--.. ", "0": "----- ", "1": ".---- ", "2": "..--- ", "3": "...-- ",
+                 "4": "....- ", "5": "..... ", "6": "-.... ", "7": "--... ", "8": "---.. ", "9": "----. "
+                }
+
+def main():
+    try:
+        assert len(sys.argv) == 2
+        assert type(sys.argv[1]) is str
+        assert all(c.isalnum() or c == ' ' for c in sys.argv[1])
+
+        S = sys.argv[1].upper()
+        res = ' '.join(NESTED_MORSE[c] for c in S)
+        print(res)
+    except:
+        print("AssertionError: the arguments are bad")
+
+if __name__ == "__main__":
+    main()
