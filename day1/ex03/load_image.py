7d339dd3b722b0dc8d649a9dafbdf79b3515d60e
Aurore DUCOBU <aducobu@paul-f3Ar5s3.clusters.42paris.fr>
2024-09-12 16:00:00 +0200

    ex03

--- day1/ex03/load_image.py
+++ day1/ex03/load_image.py
@@ -9,5 +9,8 @@ def ft_load(path: str) -> list:
         return
     array = np.array(img)
     print("The shape of image is:", array.shape)
+    print(array[0,:3])
+    print("...")
+    print(array[-1,-3:])
     return array
 
--- day1/ex03/zoom.py
+++ day1/ex03/zoom.py
@@ -1,28 +1,45 @@
 from load_image import ft_load
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 def zoom(array: list):
     center_x, center_y = array.shape[1] // 2, array.shape[0] // 2
-    zoom_size = 100
+    zoom_size = 400
     zoomed_image = array[center_y - zoom_size//2:center_y + zoom_size//2,
                          center_x - zoom_size//2:center_x + zoom_size//2, :]
-    print("new shape :", zoomed_image.shape)
-    return zoomed_image
+    gray_image = Image.fromarray(zoomed_image).convert('L')
+    zoomed_image_gray = np.array(gray_image)
+    new_arr = zoomed_image_gray.reshape(zoom_size, zoom_size, 1)
+    print("New shape after slicing:", new_arr.shape, "or", zoomed_image_gray.shape)
+    np.set_printoptions(threshold=6, edgeitems=3, formatter={'int': '{:3}'.format})
+    print(new_arr[:1])
+    return new_arr
 
 def main():
-    arr = ft_load("../landscape.jpg")
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-
-    axes[0].imshow(arr)
-    axes[0].set_title('Original Image')
-
-    zoom_image = zoom(arr)
-
-    axes[1].imshow(zoom_image)
-    axes[1].set_title('Zoomed Image')
-    
-    plt.tight_layout()
-    plt.show()
+    try:
+        arr = ft_load("../animal.jpeg")
+    except:
+        print("Error: something went wrong while loading the image.")
+        return
+    try:    
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        zoom_image = zoom(arr)
+    except:
+        
+        print("Error: something went wrong while zoomig.")
+        return
+    try:
+        axes[0].imshow(arr)
+        axes[0].set_title('Original Image')
+        axes[1].imshow(zoom_image, cmap='gray')
+        axes[1].set_title('Zoomed Image')
+        
+        plt.tight_layout()
+        plt.show()
+    except:
+        print("Error: something went wrong while displaying the image.")
+        return
 
 if __name__ == "__main__":
     main()
