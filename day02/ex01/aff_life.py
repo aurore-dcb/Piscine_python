52a777ea2d9e708b09f82ac54d23139b81488e5f
aurore-dcb <auroreducobu@gmail.com>
2025-06-05 19:48:30 +0200

    day02 in progress

---
+++ day02/ex01/aff_life.py
@@ -0,0 +1,24 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    aff_life('France')
+
+def aff_life(country: str):
+    """Display a graph with information about a country."""
+    data = load("../life_expectancy_years.csv")
+    if data is not None:
+        colonnes = data.columns.to_numpy().astype(np.int64)
+        country_data = data.loc[country].to_numpy()
+        plt.title(f"{country} Life expectancy Projections")
+        plt.plot(colonnes, country_data)
+        plt.xlabel('Year')
+        plt.ylabel('Life expectancy')
+        plt.show()
+    else:
+        print("No data available.")
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ day02/ex01/load_csv.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """Load a dataset from a CSV file \
+        and write the dimensions of the dataset."""
+    try:
+        assert isinstance(path, str), "Path must be a string"
+        assert path.endswith('.csv'), "Path must end with .csv"
+    except AssertionError as e:
+        print(f"Error: {e}")
+        return None
+    try:
+        with open(path, 'r') as file:
+            data = pd.read_csv(file, index_col=0)
+        print(f"Loading dataset of dimensions {data.shape}")
+        return data
+    except Exception as e:
+        print(f"Error loading dataset: {e}")
+        return None
